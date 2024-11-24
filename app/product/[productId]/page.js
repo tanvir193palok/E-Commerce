2c809a74e44c9aa55dd65f91bf0e01ea44518c00
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2024-11-25 03:40:34 +0600

    Added related products

--- app/product/[productId]/page.js
+++ app/product/[productId]/page.js
@@ -1,18 +1,21 @@
 import ImageGallery from "@/components/product/ImageGallery";
 import ProductDescription from "@/components/product/ProductDescription";
+import RelatedProducts from "@/components/product/RelatedProducts";
 
 const ProductPage = () => {
   return (
-    <div className="px-28 py-10">
-      <p>
+    <>
+      <p className="px-28 pt-10">
         Feature Product / <span className="text-primary">New Arrival</span>
       </p>
 
-      <div className="flex pt-4 gap-28">
+      <div className="flex px-28 pt-4 gap-28">
         <ImageGallery />
         <ProductDescription />
       </div>
-    </div>
+
+      <RelatedProducts />
+    </>
   );
 };
 

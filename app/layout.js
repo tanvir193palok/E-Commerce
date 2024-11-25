a06bbbe0f1a7497ee87e5d8be747b6aa969712bc
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2024-11-25 14:43:45 +0600

    Modified layout

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
-
+import Header from "@/components/header/Header";
 import "./globals.css";
-import Header from "@/components/header/header";
+
 import Footer from "@/components/footer/Footer";
 
 export const metadata = {
